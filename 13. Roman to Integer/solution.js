5a49990326d5707103e38fa3a214400abb2b5ee7
bax <dev.lasota@gmail.com>
2022-06-24 22:54:23 +0200

    add readme

---
+++ 13. Roman to Integer/solution.js
@@ -0,0 +1,21 @@
+var romanToInt = function (s) {
+  const romanSymbols = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  let result = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (romanSymbols[s[i + 1] > romanSymbols[s[i]]]) {
+      result += romanSymbols[s[i + 1]] - romanSymbols[s[i]];
+    } else {
+      result += romanSymbols[s[i]];
+    }
+  }
+  return result;
+};
