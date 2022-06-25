097c69ed2235e3b33d25ad288deea424f3226542
bax <dev.lasota@gmail.com>
2022-06-25 11:22:04 +0200

    fix bug with string.length == 2

--- 9.Palindrome Number/solution.js
+++ 9.Palindrome Number/solution.js
@@ -3,16 +3,21 @@
  * @return {boolean}
  */
 var isPalindrome = function (x) {
-  const string = x.toString();
-  if (string.length == 1) return true;
-  const start = string[0];
-  const end = string[string.length - 1];
-  if (start != end) {
-    return false;
-  }
-  if ((string.lenght = 2)) {
+  const xString = x.toString();
+  if (xString.length == 1) {
     return true;
   }
-  const subString = string.substring(1, string.length - 1);
+  const start = xString[0];
+  // console.log("start ", start);
+  const end = xString[xString.length - 1];
+  // console.log("end ", end);
+  if (start != end) {
+    console.log("start!=end");
+    return false;
+  }
+  if (xString.lenght == 2) {
+    return true;
+  }
+  const subString = xString.substring(1, xString.length - 1);
   return isPalindrome(subString);
 };
