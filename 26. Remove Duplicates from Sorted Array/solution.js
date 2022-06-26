d648cf68aa2b1a8060e44a4f01ceb90a30316ef4
bax <dev.lasota@gmail.com>
2022-06-26 23:34:55 +0200

    add solution

---
+++ 26. Remove Duplicates from Sorted Array/solution.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+  let j = 0;
+
+  for (i = 0; i < nums.length; i++) {
+    if (nums[i] != nums[i + 1]) {
+      nums[j++] = nums[i];
+    }
+  }
+
+  return j;
+};
