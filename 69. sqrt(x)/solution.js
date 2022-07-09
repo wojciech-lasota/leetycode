a08e55d7f24b409109534e70b668d74e35e0c919
bax <dev.lasota@gmail.com>
2022-07-09 21:25:45 +0200

    improved time complexity

--- 69. sqrt(x)/solution.js
+++ 69. sqrt(x)/solution.js
@@ -2,6 +2,19 @@
  * @param {number} x
  * @return {number}
  */
-var mySqrt = function (x) {
-  return Math.floor(Math.sqrt(x));
-};
+ var mySqrt = function(x) {
+    
+    let start =0;
+     let end = x;
+     let wanted =0;
+     while(start <= end){
+         let middle = Math.floor(start+(end-start)/2)
+         if(middle*middle<=x){
+             wanted = middle
+             start = middle+1
+         }else{
+             end = middle -1
+         }
+     }
+     return wanted
+ };
