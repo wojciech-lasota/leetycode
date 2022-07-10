624de9f08518895f68395d7b54c815c9951415d7
bax <dev.lasota@gmail.com>
2022-07-10 10:14:55 +0200

    improved runtime, faster than 96.75% of online submissions

--- 69. sqrt(x)/solution.js
+++ 69. sqrt(x)/solution.js
@@ -2,19 +2,36 @@
  * @param {number} x
  * @return {number}
  */
+//  var mySqrt = function(x) {
+    
+//     let start =0;
+//      let end = x;
+//      let wanted =0;
+//      while(start <= end){
+//          let middle = Math.floor(start+(end-start)/2)
+//          if(middle*middle<=x){
+//              wanted = middle
+//              start = middle+1
+//          }else{
+//              end = middle -1
+//          }
+//      }
+//      return wanted
+//  };
+/**
+ * @param {number} x
+ * @return {number}
+ */
  var mySqrt = function(x) {
     
-    let start =0;
-     let end = x;
-     let wanted =0;
-     while(start <= end){
-         let middle = Math.floor(start+(end-start)/2)
-         if(middle*middle<=x){
-             wanted = middle
-             start = middle+1
-         }else{
-             end = middle -1
-         }
+    if(x ==0 || x ==1) return x
+     
+     let approximation1 = x-1
+     let approximation2 = ((x/approximation1)+approximation1)/2
+     while(Math.abs(approximation1 - approximation2) > 1){
+         approximation1 = approximation2
+         approximation2 = ((x/approximation2) + approximation2)/2
      }
-     return wanted
+     
+     return Math.floor(approximation2)
  };
